6e1d225c94a2eaf6fa37207d1b00672f138484d3
Viniciu V. Buchara <vbuchara31@gmail.com>
2022-04-02 19:12:59 -0400

    Added a lot of favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,7 +38,13 @@ export default class MyDocument extends Document {
                       href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap"
                       rel="stylesheet"
                     />
-                    <link rel="icon" href='/favicon.svg' type="image/svg+xml" />
+                    <link rel="icon" type="image/png" href="/favicon16x16.png" sizes="16x16" />
+                    <link rel="icon" type="image/png" href="/favicon32x32.png" sizes="32x32" />
+                    <link rel="icon" type="image/png" href="/favicon96x96.png" sizes="96x96" />
+
+                    <link rel="apple-touch-icon" sizes="72x72" href="/favicon72x72.png" />
+                    <link rel="apple-touch-icon" sizes="114x114" href="/favicon114x114.png" />
+                    <link rel="apple-touch-icon" sizes="152x152" href="/favicon152x152.png" />
                 </Head>
                 <body>
                     <Main />
